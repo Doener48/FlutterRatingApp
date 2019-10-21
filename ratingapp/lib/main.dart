d7917a59a4a70fe657bf075824cf325baae028df
unknown <doener48.1@web.de>
2019-10-21 22:06:07 +0200

    test layout 1

--- ratingapp/lib/main.dart
+++ ratingapp/lib/main.dart
@@ -2,19 +2,62 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+final titles = ['Bier', 'Film', 'Serie', 'Rezept', ' ... '];
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'RatingApp',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Rating App'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
+        title: 'RatingApp',
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: 'Navigation menu',
+              onPressed: null,
+            ),
+            title: Text('Flutter Rating App'),
+          ),
+          body: Center(
+              child: CustomScrollView(
+            primary: false,
+            slivers: <Widget>[
+              SliverPadding(
+                padding: const EdgeInsets.all(20),
+                sliver: SliverGrid.count(
+                  crossAxisSpacing: 5,
+                  mainAxisSpacing: 5,
+                  crossAxisCount: 4,
+                  children: _builder1(),
+                ),
+              ),
+            ],
+          )),
+        ));
+  }
+}
+
+List<Widget> _builder1() {
+  int itemCount = titles.length;
+  List<Widget> ret = [];
+  for (var i = 0; i < itemCount; i++) {
+    ret.add(
+      Card(
+        child: InkWell(
+          onTap: () {
+            print(titles[i] + " tapped");
+          },
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            child: Center(
+              child: Text(titles[i]),
+            ),
+            color: Colors.teal[100],
+          ),
         ),
       ),
     );
   }
+  return ret;
 }
