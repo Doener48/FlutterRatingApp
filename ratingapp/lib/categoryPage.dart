9b9472eaa6b0bfd846de73b5e3c0a3aaaa3cba04
unknown <doener48.1@web.de>
2019-10-23 00:40:12 +0200

    page navigation

---
+++ ratingapp/lib/categoryPage.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import 'itemPage.dart';
+
+final titles = ['Bier', 'Film', 'Serie', 'Rezept', ' ... '];
+
+class CategoryPage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter Rating App'),
+          ),
+          body: Center(
+              child: CustomScrollView(
+            semanticChildCount: 5,
+            slivers: <Widget>[
+              SliverGrid(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 5,
+                  mainAxisSpacing: 5,
+                ),
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    return Card(
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(_createRoute(titles[index]));
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          child: Center(
+                            child: Text(titles[index]),
+                          ),
+                          color: Colors.teal[100],
+                        ),
+                      ),
+                    );
+                  },
+                  childCount: titles.length,
+                ),
+              ),
+            ],
+          )),
+        ));
+  }
+}
+
+Route _createRoute(String title) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => ItemPage(title),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return child;
+    },
+  );
+}
